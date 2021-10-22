de82ecf7b07d46b228fb0284c44773513e476b4a
mbence01 <madranszkibence@gmail.com>
2021-10-22 14:22:20 +0200

    move menu to menu.php

---
+++ sites/menu.php
@@ -0,0 +1,22 @@
+<div id="header" class="container-fluid">
+    <div class="row">
+        <table>
+            <tr>
+                <td class="d-none d-lg-inline d-md-inline col-lg-4 col-md-2">
+                    <a id="main-logo" href="/index.php">
+                        <img src="/img/logo.png" width="64" align="absmiddle" title="Mucsaröcsöge Közlekedési Társaság">
+                        <span class="d-none d-lg-inline" id="main-title">Mucsaröcsöge Közlekedési Társaság</span>
+                    </a>
+                </td>
+                <td class="col-lg-8 col-md-10 col-sm-12" id="header-right">
+                    <span class="d-inline-block d-md-none d-sm-block" id="menu-title">Menü</span>
+                    <a href="#" class="d-inline-block d-md-none d-sm-block"><img src="/img/logo.png" width="32"> Főoldal</a>
+                    <a href="#" class="d-md-inline-block d-sm-block"><img src="/img/bus.png" width="25"> Járatok</a>
+                    <a href="#" class="d-md-inline-block d-sm-block"><img src="/img/busstop.png" width="27"> Megállók</a>
+                    <a href="#" class="d-md-inline-block d-sm-block"><img src="/img/route.png" width="25"> Útvonaltervezés</a>
+                    <a href="#" class="d-md-inline-block d-sm-block"><img src="/img/other.png" width="35"> Továbbiak</a>
+                </td>
+            </tr>
+        </table>
+    </div>
+</div>

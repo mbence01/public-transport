47c142cf16a8b406628621bee2794023c5b885bd
mbence01 <madranszkibence@gmail.com>
2021-10-22 07:11:32 +0200

    added main menu

---
+++ index.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+        <title>Főoldal</title>
+
+        <link rel="stylesheet" href="/css/bootstrap/bootstrap.css" />
+        <link rel="stylesheet" href="/css/main.css" />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <div id="header" class="container-fluid">
+            <div class="row">
+                <table>
+                    <tr>
+                        <td class="col-xs-4">
+                            <a id="main-logo" href="/index.php">
+                                <img src="/img/logo.png" width="64" align="absmiddle">
+                                <span id="main-title">Mucsaröcsöge Közlekedési Társaság</span>
+                            </a>
+                        </td>
+                        <td class="col-xs-8" id="header-right">
+                            <a href="#"><img src="/img/bus.png" width="25"> Járatok</a>
+                            <a href="#"><img src="/img/busstop.png" width="27"> Megállók</a>
+                            <a href="#"><img src="/img/route.png" width="25"> Útvonaltervezés</a>
+                            <a href="#"><img src="/img/other.png" width="35"> Továbbiak</a>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </body>
+</html>

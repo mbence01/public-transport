cb95338c4090b5682440d0b6fc9cd52ad02e5070
mbence01 <h048514@stud.u-szeged.hu>
2021-10-25 16:53:29 +0200

    included jquery

--- index.php
+++ index.php
@@ -8,6 +8,8 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
+
+        <script type="text/javascript" src="js/jquery/jquery.js"></script>
     </head>
     <body>
         <?php
